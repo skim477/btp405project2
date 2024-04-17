b0546844c56e8295c675b5d6a013c403a15cd349
AdielRoyt <adielroytgarts@gmail.com>
2024-04-16 20:51:18 -0400

    added unit test for UserProfile

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="bob", password="1234")
+        self.user2 = User.objects.create(username="Rob", password="5432")
+        UserProfile.objects.create(self.user)
+        UserProfile.objects.create(self.user2)
+
+    def test_role(self):
+        userprofile = UserProfile.objects.get(user=self.user)
+        userprofile2 = UserProfile.objects.get(user=self.user2)
+        self.assertEqual(userprofile.role, "student")
+        self.assertEqual(userprofile2.role, "student")
